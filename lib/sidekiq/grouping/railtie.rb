3488825903f8c20be0117f897aa868c431fe9b89
Daniel Green <dan@shup.com>
2022-04-11 21:48:13 -0700

    Create railtie.rb

---
+++ lib/sidekiq/grouping/railtie.rb
@@ -0,0 +1,9 @@
+module Sidekiq
+  module Grouping
+    class Railtie < ::Rails::Railtie
+      config.after_initialize do
+        Sidekiq::Grouping.start! if Sidekiq.server?
+      end
+    end
+  end
+end
